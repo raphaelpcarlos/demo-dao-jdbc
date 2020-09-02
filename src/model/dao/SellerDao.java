cd79e48e3925646e7a73ba1be2275f80c65b573e
raphael carlos <raphaelpereiracarlos@gmail.com>
2020-09-02 14:35:25 -0300

    Project created

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,16 @@
 package model.dao;
+import java.util.List;
+import model.entities.Seller;
 
 public interface SellerDao {
 
+		void insert(Seller obj);
+		
+		void update(Seller obj);
+
+		void delete(Integer id);
+
+		Seller findById(Integer id);
+		
+		List<Seller> findAll();
 }
