4c0a3e4ee83a546047d71328b1f05a68b03e3dee
raphael carlos <raphaelpereiracarlos@gmail.com>
2020-09-02 14:28:06 -0300

    Project created

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,17 @@
+package model.dao;
+import java.util.List;
+import model.entities.Departament;
+
+public interface DepartmentDao {
+
+	void insert(Departament obj);
+	
+	void update(Departament obj);
+
+	void delete(Integer id);
+
+	Departament findById(Integer id);
+	
+	List<Departament> findAll();
+	 
+}
