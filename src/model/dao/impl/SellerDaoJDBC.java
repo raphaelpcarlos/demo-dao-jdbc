b2488c63f0331c6d6b363da572118278c226686f
raphael carlos <raphaelpereiracarlos@gmail.com>
2020-09-02 20:09:36 -0300

    delete implementatio

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -97,8 +97,25 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			int rows = st.executeUpdate();
+			
+			if(rows == 0) {
+				throw new DbException("ID inexistente");
+			}
+			
+		}catch (SQLException e) {
 
+		throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
+		
 	}
 
 	@Override
